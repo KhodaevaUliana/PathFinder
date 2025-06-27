ef06e9250f377a355c5558957e6c3cee3900efcd
KhodaevaUliana <uliana.khodaeva@gmail.com>
2025-06-27 19:14:45 +0200

    Updated the shortest path's finder for real OSM data. Added corresponding integration tests

--- src/main/java/com/example/PathFinder/Graph.java
+++ src/main/java/com/example/PathFinder/Graph.java
@@ -18,6 +18,7 @@ public class Graph {
         this.adjacencyList = adjacencyList;
     }
 
+
     public static Path shortestPath (Graph graph, long startNodeID, long finishNodeID) throws IllegalArgumentException {
         Set<Node> nodes = graph.getAdjacencyList().keySet();
         HashMap<Node, Double> distances = new HashMap<>();
@@ -84,6 +85,51 @@ public class Graph {
         long finishNodeID = Long.parseLong(finishNodeIDString);
         return shortestPath(graph, startNodeID, finishNodeID);
     }
+
+    public static Path shortestPath (Graph graph, Node startNode, Node finishNode)  {
+        HashMap<Node, Double> distances = new HashMap<>();
+
+        distances.put(startNode, 0.0);
+
+        PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparingDouble(distances::get));
+        HashMap<Node, Node> previous = new HashMap<>();
+        queue.add(startNode);
+
+
+        //Dijkstra algorithm
+        while (!queue.isEmpty()) {
+            Node currentNode = queue.poll();
+            if (currentNode.equals(finishNode)) {
+                break;
+            }
+            for (Edge edge : graph.getAdjacencyList().get(currentNode)) {
+                Node neighbour = edge.getTo();
+                double newDistance = distances.get(currentNode) + edge.getDistance();
+                if (newDistance < distances.getOrDefault(neighbour, Double.POSITIVE_INFINITY)) {
+                    distances.put(neighbour, newDistance);
+                    previous.put(neighbour, currentNode);
+                    queue.add(neighbour);
+                }
+            }
+        }
+        if (distances.getOrDefault(finishNode, Double.POSITIVE_INFINITY) == Double.POSITIVE_INFINITY) {
+            return new Path(new ArrayList<>(), Double.POSITIVE_INFINITY);
+        }
+
+        //trace the path back
+        ArrayList<Node> path = new ArrayList<>();
+        path.add(finishNode);
+        Node currNode = finishNode;
+        while (!currNode.equals(startNode)) {
+            Node nextNode = previous.get(currNode);
+            path.addFirst(nextNode);
+            currNode = nextNode;
+        }
+        return new Path(path, distances.get(finishNode));
+
+
+    }
+
 
 
 
