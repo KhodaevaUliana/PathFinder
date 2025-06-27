b10703e3cd45e8e9c4be63ff93e8ca3c14600286
KhodaevaUliana <uliana.khodaeva@gmail.com>
2025-06-27 15:12:56 +0200

    Added Spring Service providing a graph from an OSM-file

--- backend/src/main/java/com/example/PathFinder/GraphProvider.java
+++ backend/src/main/java/com/example/PathFinder/GraphProvider.java
@@ -1,11 +1,31 @@
 package com.example.PathFinder;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.net.URISyntaxException;
+
+import java.util.*;
+
+@Service
 public class GraphProvider {
     private Graph graph;
 
-    public GraphProvider(String path) {
-        OsmParser parser = new OsmParser(path);
-        Graph graph = new Graph(parser.getAdjacencyList());
+    @PostConstruct
+    public void init() {
+        try {
+            java.nio.file.Path path = java.nio.file.Paths.get(Objects.requireNonNull(getClass()
+                            .getClassLoader()
+                            .getResource("monaco-latest.osm.pbf"))
+                    .toURI());
+
+            String filePath = path.toString();
+            OsmParser parser = new OsmParser(filePath);
+            this.graph = new Graph(parser.getAdjacencyList());
+        } catch (URISyntaxException exception) {
+            System.out.println(exception.getMessage());
+        }
+
     }
 
     public Graph getGraph() {
