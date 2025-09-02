dba59e03d52777c2111fc9f8362bb0b379c9e5e0
KhodaevaUliana <uliana.khodaeva@gmail.com>
2025-09-02 23:31:14 +0200

    UX improved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Polyline, Marker, Popup, useMapEvents } from 'react-leaflet';
+import { MapContainer, TileLayer, Polyline, Marker, Popup, useMapEvents, Tooltip } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
@@ -21,7 +21,7 @@ function ClickHandler({onClick}) {
   useMapEvents({
     click(e) {  //e is a click event object (LeafletMouseEvent)
       onClick(e.latlng); //latitude (lat) and longitude (lng) of a click event object
-      console.log("Event object:", e);
+      //console.log("Event object:", e);
     }
   });
   return null;
@@ -31,6 +31,10 @@ function ClickHandler({onClick}) {
 function App() {
   const [route, setRoute] = useState([]);
   const [points, setPoints] = useState([]);
+  const [distance, setDistance] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+
 
   const handleMapClick = (latlng) => {
     const newPoint = {
@@ -49,6 +53,8 @@ function App() {
   const handleRefresh = () => {
     setPoints([]);
     setRoute([]);
+    setDistance(null);
+    setErrorMessage(null);
   };
 
   useEffect(() => {
@@ -58,24 +64,42 @@ function App() {
       fetch(`http://localhost:8080/route?startLatitude=${start.lat}&startLongitude=${start.lng}&finishLatitude=${finish.lat}&finishLongitude=${finish.lng}`)
         .then(res => res.json())
         .then(data => {
-          console.log("Fetched data:", data);
+          //console.log("Fetched data:", data);
           setRoute(data.nodes);
+          setDistance(data.distance);
+          setErrorMessage(null);
         })
-        .catch(err => console.error(err));
+        .catch(err => {
+            console.error(err);
+            setRoute([]);
+            setDistance(null);
+            setErrorMessage("The route between these points cannot be found. Please, change start or/and finish point.");
+        }
+       );
       }
     }, [points]);
 
 
   const positions = route.map(node => [node.latitude, node.longitude]);
 
+  let message;
+  if (points.length === 2 && distance !== null) {
+    message = `The distance is ${Math.round(distance/10)/100} km. Press Refresh to start over.`;
+  } else if (points.length === 0) {
+    message = "Click on the start point";
+  } else {
+    message = "Click on the finish point";
+  }
+
+
   return (
       <div>
         <h1>Let's explore Monaco together! </h1>
-        <h2> Click two points to see a route </h2>
+        <h2> {errorMessage ? errorMessage : message } </h2>
         <button onClick={handleRefresh} style={{ margin: '20px', color: 'magenta', fontWeight: 'bold', fontSize: '20px' }}>
           Refresh
         </button>
-        <MapContainer center={[43.735, 7.42]} zoom={15} style={{ height: '90vh', width: '100%' }}>
+        <MapContainer center={[43.735, 7.42]} zoom={15} style={{ height: '70vh', width: '100%' }}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution="&copy; OpenStreetMap contributors"
@@ -84,6 +108,9 @@ function App() {
           {points.map((pt, idx) => (
             <Marker key={idx} position={[pt.lat, pt.lng]}>
               <Popup>{idx === 0 ? 'Start' : 'Finish'}</Popup>
+               <Tooltip permanent direction="top">
+                  {idx === 0 ? 'Start' : 'Finish'}
+               </Tooltip>
             </Marker>
           ))}
           {positions.length > 0 && (
