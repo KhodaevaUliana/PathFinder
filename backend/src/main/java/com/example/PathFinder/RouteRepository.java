c85001d76b871616793cf4d3cdf586633c13e66b
KhodaevaUliana <uliana.khodaeva@gmail.com>
2025-09-12 14:31:33 +0200

    Added route repository

---
+++ backend/src/main/java/com/example/PathFinder/RouteRepository.java
@@ -0,0 +1,21 @@
+package com.example.PathFinder;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public interface RouteRepository extends JpaRepository<Route, Long> {
+
+    Optional<Route> findByUsernameAndRouteName(String username, String routeName);
+
+    boolean existsByUsernameAndRouteName(String username, String routeName);
+
+    void deleteByUsernameAndRouteName(String username, String routeName);
+
+    @Query("SELECT route.routeName FROM Route route WHERE route.username = :username")
+    ArrayList<String> findRouteNamesByUsername(@Param("username") String username);
+
+}
