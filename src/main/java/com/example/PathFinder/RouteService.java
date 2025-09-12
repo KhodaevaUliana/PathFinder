51d28d65c143831ce47de392c4e23f013e65eb8e
KhodaevaUliana <uliana.khodaeva@gmail.com>
2025-09-12 14:34:49 +0200

    Added route service

---
+++ src/main/java/com/example/PathFinder/RouteService.java
@@ -0,0 +1,46 @@
+package com.example.PathFinder;
+
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class RouteService {
+
+    private final RouteRepository routeRepository;
+
+    public RouteService (RouteRepository routeRepository) {
+        this.routeRepository = routeRepository;
+    }
+
+    //save the route in the DB
+    public Route saveRoute(Route route) {
+        try {
+            return routeRepository.save(route);
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("A route with this name already exists. Choose another name.", e);
+        }
+    }
+
+    //fetch routes' names for a given user
+    public ArrayList<String> fetchRouteNamesByUsername(String username) {
+        return routeRepository.findRouteNamesByUsername(username);
+    }
+
+    //find a route by a username and a name
+    public Route fetchRouteByUsernameAndName(String username, String name) {
+        return routeRepository.findByUsernameAndRouteName(username, name).orElse(null);
+    }
+
+    //delete route (return true if success, false if there was no such a route)
+    public boolean deleteRoute(String username, String name) {
+        if (!routeRepository.existsByUsernameAndRouteName(username, name)) {
+            return false;
+        }
+        routeRepository.deleteByUsernameAndRouteName(username, name);
+        return true;
+    }
+
+}
