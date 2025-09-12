e66a2e61d78cc13bb4f620607669e7e194f14f9e
KhodaevaUliana <uliana.khodaeva@gmail.com>
2025-09-12 14:41:55 +0200

    Refactoring

--- backend/src/main/java/com/example/PathFinder/repository/RouteRepository.java
+++ backend/src/main/java/com/example/PathFinder/repository/RouteRepository.java
@@ -1,5 +1,6 @@
-package com.example.PathFinder;
+package com.example.PathFinder.repository;
 
+import com.example.PathFinder.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
