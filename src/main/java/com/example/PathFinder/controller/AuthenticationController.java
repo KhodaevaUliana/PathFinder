a93c2c0c7dd5bff6eb70755eb5178976342efade
KhodaevaUliana <uliana.khodaeva@gmail.com>
2025-09-17 21:29:58 +0200

    AuthenticationController refactored

--- src/main/java/com/example/PathFinder/controller/AuthenticationController.java
+++ src/main/java/com/example/PathFinder/controller/AuthenticationController.java
@@ -27,38 +27,26 @@ public class AuthenticationController {
     //sign-up -- create a user
     @PostMapping("/signup")
     public ResponseEntity<String> signUp (@RequestBody LogInCredentials logInCredentials) {
-        if (userDetailsManager.userExists(logInCredentials.getUsername())) {
-            return ResponseEntity.badRequest().body("User with this username already exists.");
+        try {
+            authenticationService.signUp(logInCredentials);
+        } catch (IllegalArgumentException error) {
+            return ResponseEntity.badRequest().body(error.getMessage());
+        } catch (Exception error) {
+            return ResponseEntity.internalServerError().body(error.getMessage());
         }
-        if (logInCredentials.getUsername().length() > 50) {
-            return ResponseEntity.badRequest().body("The username is too long (> 50 characters).");
-        }
-        if (logInCredentials.getUsername().isEmpty()) {
-            return ResponseEntity.badRequest().body("Please enter a non-empty username.");
-        }
-        if (logInCredentials.getPassword().isEmpty()) {
-            return ResponseEntity.badRequest().body("Please enter a password.");
-        }
-        UserDetails newUser = User.withUsername(logInCredentials.getUsername())
-                .password(passwordEncoder.encode(logInCredentials.getPassword()))
-                .build();
-        userDetailsManager.createUser(newUser);
-
         return ResponseEntity.ok("User created successfully.");
     }
 
     //log in -- get a JWT token!
     @PostMapping("/login")
     public ResponseEntity<String> logIn (@RequestBody LogInCredentials logInCredentials) {
-        if (!userDetailsManager.userExists(logInCredentials.getUsername())) {
-            return ResponseEntity.badRequest().body("Wrong password or username");
-        }
-        UserDetails user = userDetailsManager.loadUserByUsername(logInCredentials.getUsername());
-        if (passwordEncoder.matches(logInCredentials.getPassword(), user.getPassword())) {
-            String token = JWTUtility.generateToken(user.getUsername());
+        try {
+            String token = authenticationService.logIn(logInCredentials);
             return ResponseEntity.ok(token);
-        } else {
-            return ResponseEntity.badRequest().body("Wrong password or username");
+        } catch (IllegalArgumentException error) {
+            return ResponseEntity.badRequest().body(error.getMessage());
+        } catch (Exception error) {
+            return ResponseEntity.internalServerError().body(error.getMessage());
         }
     }
 
