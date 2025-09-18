1e8d2b42416ec62e5ec4a60071c91373953543fb
KhodaevaUliana <uliana.khodaeva@gmail.com>
2025-09-18 18:06:08 +0200

    Secret key moved from JWTUtility

--- src/main/java/com/example/PathFinder/config/JWTConfig.java
+++ src/main/java/com/example/PathFinder/config/JWTConfig.java
@@ -1,18 +1,29 @@
 package com.example.PathFinder.config;
 
 import com.example.PathFinder.util.JWTUtility;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 
 @Configuration
 public class JWTConfig {
 
     @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withSecretKey(JWTUtility.getKey()).build();
+    public SecretKey jwtSigningKey() {
+        return Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(SecretKey jwtSigningKey) {
+        return NimbusJwtDecoder.withSecretKey(jwtSigningKey).build();
     }
 
 }
