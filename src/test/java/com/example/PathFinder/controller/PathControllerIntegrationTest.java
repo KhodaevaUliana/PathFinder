7f0470082b8376d73b015d0bac62662a03d1d446
KhodaevaUliana <uliana.khodaeva@gmail.com>
2025-09-19 18:34:19 +0200

    PathControllerIntegrationTests added

--- src/test/java/com/example/PathFinder/controller/PathControllerIntegrationTest.java
+++ src/test/java/com/example/PathFinder/controller/PathControllerIntegrationTest.java
@@ -13,31 +13,87 @@ import org.springframework.http.MediaType;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Disabled
-@SpringBootTest
+
+@SpringBootTest(properties = "spring.profiles.active=test")
 @AutoConfigureMockMvc
 public class PathControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void getRouteRealGraph_success() throws Exception {
-        // Pick coordinates that exist in your Monaco PBF file
-        double startLat = 43.7384;
-        double startLon = 7.4246;
-        double finishLat = 43.7400;
-        double finishLon = 7.4260;
+    public void getPathRealGraphExactNodesSuccess() throws Exception {
+        //start at Marienplatz, finish at Chinesischem Turm
+        //no exact coincidence with the nodes
+        double startLatitude = 48.137110;
+        double startLongitude = 11.575410;
+        double finishLatitude = 48.1585;
+        double finishLongitude = 11.6035;
 
         mockMvc.perform(get("/route")
-                        .param("startLatitude", String.valueOf(startLat))
-                        .param("startLongitude", String.valueOf(startLon))
-                        .param("finishLatitude", String.valueOf(finishLat))
-                        .param("finishLongitude", String.valueOf(finishLon))
+                        .param("startLatitude", String.valueOf(startLatitude))
+                        .param("startLongitude", String.valueOf(startLongitude))
+                        .param("finishLatitude", String.valueOf(finishLatitude))
+                        .param("finishLongitude", String.valueOf(finishLongitude))
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.nodes").isArray())
                 .andExpect(jsonPath("$.nodes.length()").value(org.hamcrest.Matchers.greaterThan(1)));
     }
+    @Test
+    public void getPathRealGraphNoExactNodesSuccess() throws Exception {
+        //start near Marienplatz, finish near Chinesischem Turm
+        //no exact coincidence with the nodes
+        double startLatitude = 48.137112;
+        double startLongitude = 11.575410;
+        double finishLatitude = 48.1585;
+        double finishLongitude = 11.6036;
+
+        mockMvc.perform(get("/route")
+                        .param("startLatitude", String.valueOf(startLatitude))
+                        .param("startLongitude", String.valueOf(startLongitude))
+                        .param("finishLatitude", String.valueOf(finishLatitude))
+                        .param("finishLongitude", String.valueOf(finishLongitude))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nodes").isArray())
+                .andExpect(jsonPath("$.nodes.length()").value(org.hamcrest.Matchers.greaterThan(1)));
+    }
+
+    @Test
+    public void getPathRealGraphOutsideMap() throws Exception {
+        //start near Marienplatz, finish outside map
+        double startLatitude = 48.137112;
+        double startLongitude = 11.575410;
+        double finishLatitude = 60.0;
+        double finishLongitude = 11.6036;
+
+        mockMvc.perform(get("/route")
+                        .param("startLatitude", String.valueOf(startLatitude))
+                        .param("startLongitude", String.valueOf(startLongitude))
+                        .param("finishLatitude", String.valueOf(finishLatitude))
+                        .param("finishLongitude", String.valueOf(finishLongitude))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        public void getPathRealGraphStartAndFinishCoincide() throws Exception {
+            //start near Marienplatz, finish at the same point
+            double startLatitude = 48.137112;
+            double startLongitude = 11.575410;
+            double finishLatitude = 48.137112;
+            double finishLongitude = 11.575410;
+            mockMvc.perform(get("/route")
+                            .param("startLatitude", String.valueOf(startLatitude))
+                            .param("startLongitude", String.valueOf(startLongitude))
+                            .param("finishLatitude", String.valueOf(finishLatitude))
+                            .param("finishLongitude", String.valueOf(finishLongitude))
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.nodes").isArray())
+                    .andExpect(jsonPath("$.nodes.length()").value(org.hamcrest.Matchers.equalTo(1)));
+
+        }
 
 
 
