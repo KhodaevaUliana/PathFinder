f4b90fd70618b277f4e1167b0727dc4dd6b5e808
KhodaevaUliana <uliana.khodaeva@gmail.com>
2025-10-04 15:08:36 +0200

    Code for prod environment adjusted accordingly

--- backend/src/main/java/com/example/PathFinder/repository/OsmGraphRepositoryImplProd.java
+++ backend/src/main/java/com/example/PathFinder/repository/OsmGraphRepositoryImplProd.java
@@ -1,7 +1,11 @@
 package com.example.PathFinder.repository;
 
+import com.example.PathFinder.model.Edge;
 import com.example.PathFinder.model.Graph;
-import com.example.PathFinder.util.OsmParser;
+import com.example.PathFinder.model.Node;
+import com.example.PathFinder.util.NodeUtils;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
@@ -13,6 +17,8 @@ import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.HashMap;
+import java.util.List;
 
 
 @Repository
@@ -20,7 +26,7 @@ import java.io.FileOutputStream;
 public class OsmGraphRepositoryImplProd implements GraphRepository{
 
     @Value("${osm.graph.file}")
-    private String osmFile;
+    private String graphFile;
     private Graph graph;
 
     @PostConstruct
@@ -32,8 +38,8 @@ public class OsmGraphRepositoryImplProd implements GraphRepository{
         try {
             String filePath;
             // Parse bucket/key
-            String bucket = osmFile.split("/")[2];
-            String key = osmFile.substring(osmFile.indexOf(bucket) + bucket.length() + 1);
+            String bucket = graphFile.split("/")[2];
+            String key = graphFile.substring(graphFile.indexOf(bucket) + bucket.length() + 1);
 
             S3Client s3 = S3Client.builder().build();
             File localFile = new File("/tmp/" + key.substring(key.lastIndexOf("/") + 1));
@@ -49,8 +55,20 @@ public class OsmGraphRepositoryImplProd implements GraphRepository{
 
             filePath = localFile.getAbsolutePath();
 
-            OsmParser parser = new OsmParser(filePath);
-            this.graph = new Graph(parser.getAdjacencyList());
+            File inputFile = new File(filePath);
+            if (!inputFile.exists()) {
+                throw new Exception("file not found");
+            }
+            ObjectMapper objectMapper = new ObjectMapper();
+            HashMap<String, List<Edge>> rawAdjacencyList = objectMapper.readValue(inputFile, new TypeReference<HashMap<String, List<Edge>>>() {});
+            //now, let's deserialize nodes
+            HashMap<Node, List<Edge>> adjacencyList = new HashMap<>();
+            for (String nodeStr : rawAdjacencyList.keySet()) {
+                Node node = NodeUtils.parseNodeFromString(nodeStr);
+                adjacencyList.put(node, rawAdjacencyList.get(nodeStr));
+            }
+
+            this.graph = new Graph(adjacencyList);
 
         } catch (Exception e) {
             e.printStackTrace();
